1ef520f45822ff58a0213dd8b01730aa1284fb5e
Frank77GLD <UtCollector@gmail.com>
2012-11-23 01:18:57 +0100

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,6 @@
+# AndroidProducts.mk - specifies the appropriate device's make file, to use for building. i.e. /device/YG/m805_892x/device_m805_892x.mk, this is specific also.
+# This file lists the product definition files that define
+# configurations which are actually buildable (e.g. through lunch)
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/device_m805_892x.mk
+#   $(LOCAL_DIR)/small_tcc892x.mk
